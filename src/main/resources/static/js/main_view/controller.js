5271260cb4005dbf06174c4bafb12ab2662a1dcc
Ustinov Sergey <s.ustinov@lamoda.ru>
2018-04-29 18:52:03 +0300

    Paging support has been added

--- src/main/resources/static/js/main_view/controller.js
+++ src/main/resources/static/js/main_view/controller.js
@@ -19,18 +19,30 @@ mainModule.controller('PersonController', function ($scope, PersonService) {
         "rowsPerPage": 3
     };
 
+
+    $scope.state = {
+        isLoading : false,
+        //totalPages : 0,
+        //start : undefined,
+        currentPage : undefined
+    };
+
     $scope.settings = {
         displayedPages : 10,
         itemsByPage : 3
     };
 
     $scope.searchByFilter = function(filter) {
+        $scope.state.isLoading = true;
         PersonService.searchByFilter(filter).then(function(response) {
             if (response.result === 'OK') {
-                $scope.data = [];
-                angular.forEach(response.data, function(person){
+                $scope.data.splice(0, $scope.data.length);
+
+                angular.forEach(response.data, function(person) {
                     process(person);
                 });
+                $scope.state.isLoading = false;
+                //$scope.state.totalPages = Math.ceil($scope.data.length / $scope.settings.itemsByPage);
             } else {
                 // TODO : Implement error modal
             }
@@ -66,6 +78,47 @@ mainModule.controller('PersonController', function ($scope, PersonService) {
             return '';
         }
     };
+
+    // Handler of 'items per page change' events
+    $scope.$watch('settings.itemsByPage', function(newValue, oldValue) {
+        if (!angular.isUndefined(newValue) && newValue !== oldValue) {
+            $scope.searchByFilter($scope.filter);
+        }
+    });
+
+    // Handler of 'page changed' events
+    $scope.pageChanged = function(newPage) {
+        if (newPage !== $scope.state.currentPage) {
+            $scope.state.currentPage = newPage;
+            $scope.searchByFilter($scope.filter);
+        }
+    };
+
+    // var changeTableState = function() {
+    //     if (!angular.isUndefined(tableController)) {
+    //         var state = tableController.tableState();
+    //         state.pagination.totalItemCount = $scope.data.length;
+    //         state.pagination.numberOfPages = $scope.state.totalPages;
+    //         state.pagination.start = ($scope.state.currentPage - 1) * $scope.settings.itemsByPage;
+    //         tableController.pipe();
+    //     }
+    // };
+
+    // $scope.$watch('tableController', function(newValue, oldValue) {
+    //     if (!angular.isUndefined(newValue) && newValue !== oldValue) {
+    //         $scope.tableController.pipe = function() {
+    //             $scope.tableController.pipe();
+    //         };
+    //     }
+    // });
+
+    // $scope.pipe = function(tableState, pipeFn) {
+    //     if (tableState.pagination.start !== $scope.state.start) {
+    //         $scope.state.start = tableState.pagination.start;
+    //
+    //     }
+    //     pipeFn();
+    // };
 
     $scope.searchByFilter($scope.filter);
 });
