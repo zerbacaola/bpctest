0b7462a41e8f81928186770844a4d5bc5d7ac678
Ustinov Sergey <s.ustinov@lamoda.ru>
2018-05-03 07:38:20 +0300

    Build profiles was added

---
+++ sql/sqlite/ddl.sql
@@ -0,0 +1,36 @@
+﻿-- SERIAL IS too short for ID storing
+BEGIN;
+CREATE TABLE person (
+  id        SERIAL PRIMARY KEY NOT NULL,
+  created   TIMESTAMP WITH TIME ZONE,
+  modified  TIMESTAMP WITH TIME ZONE,
+  version   INTEGER NOT NULL DEFAULT 0,
+  name      VARCHAR (255) NOT NULL,
+  gender    VARCHAR (1) NOT NULL
+);
+CREATE INDEX idx_person_name ON person(name);
+CREATE INDEX idx_person_gender ON person(gender);
+
+CREATE TABLE person_parent (
+  person_id   BIGINT NOT NULL,
+  parent_id   BIGINT NOT NULL,
+  FOREIGN KEY (person_id) REFERENCES person(id),
+  FOREIGN KEY (parent_id) REFERENCES person(id)
+);
+
+CREATE INDEX idx_person_parent_person_id ON person_parent(person_id);
+CREATE INDEX idx_person_parent_parent_id ON person_parent(parent_id);
+
+CREATE TABLE person_photo (
+  id        SERIAL PRIMARY KEY NOT NULL,
+  created   TIMESTAMP WITH TIME ZONE,
+  modified  TIMESTAMP WITH TIME ZONE,
+  version   INTEGER NOT NULL DEFAULT 0,
+  data      BYTEA,
+  person_id BIGINT,
+  FOREIGN KEY (person_id) REFERENCES person(id)
+);
+
+CREATE INDEX idx_person_photo_person_id ON person_photo(person_id);
+
+COMMIT;
---
+++ sql/sqlite/migration.sql
@@ -0,0 +1,29 @@
+﻿BEGIN;
+INSERT INTO person(id, created, modified, version, name, gender) VALUES
+  (1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1, 'Петр', 'M'),
+  (2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1, 'Тамара', 'F'),
+  (3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1, 'Иван', 'M'),
+  (4, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1, 'Ольга', 'F');
+
+INSERT INTO person_parent(person_id, parent_id)
+SELECT p.id, r.id
+FROM person p
+  LEFT JOIN person r ON r.name = 'Иван'
+WHERE  p.name= 'Петр';
+
+INSERT INTO person_parent(person_id, parent_id)
+SELECT p.id, r.id
+FROM person p
+  LEFT JOIN person r ON r.name = 'Ольга'
+WHERE  p.name= 'Петр';
+
+INSERT INTO person_parent(person_id, parent_id)
+SELECT p.id, r.id
+FROM person p
+  LEFT JOIN person r ON r.name = 'Иван'
+WHERE  p.name= 'Тамара';
+
+INSERT INTO person_photo(id, created, modified, version, data, person_id)
+SELECT 5, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1, NULL, (SELECT id FROM person WHERE name = 'Иван');
+
+COMMIT;
